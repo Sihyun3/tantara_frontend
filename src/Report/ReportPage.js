19dda1325f2a277e8662414eb1ae31ba26bae62c
Sihyun <yellownyou@gmail.com>
2023-04-11 13:18:37 +0900

    3st commit

---
+++ src/Report/ReportPage.js
@@ -0,0 +1,10 @@
+import style from './ReportPage.module.css';
+
+function ReportPage(){
+    return (
+        <div class={container}>
+            <></>
+        </div>
+    )
+}
+export default ReportPage;
