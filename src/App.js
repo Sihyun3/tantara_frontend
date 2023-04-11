3f5b8759db2edfc5ee69bb12b5b8fc66d69a71cc
Sihyun <yellownyou@gmail.com>
2023-04-11 18:21:39 +0900

    0411_sihyun

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
 import Main from './Main/Main';
-
 import Login from './Login/Login'
 // import SignUp from './SignUp/SignUp';
 // import TipList from './TipList/TipList';
